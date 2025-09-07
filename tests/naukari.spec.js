b601d51056cac545411c41f6ef30813723ac22b9
Rakesh <rakeshgowda3937@gmail.com>
2025-09-07 12:12:17 +0530

    Initial commit - project setup

---
+++ tests/naukari.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+test('Early Access Automation', async ({ page }) => {
+    test.slow();
+    await page.goto('https://www.naukri.com/');
+    await page.locator('//a[text()="Login"]').click();
+    await page.getByPlaceholder('Enter your active Email ID / Username').fill('rakeshgowda3937@gmail.com');
+    await page.getByPlaceholder('Enter your password').fill('Rakesh@0412');
+    await page.getByRole('button', { name: 'Login', exact: true }).click();
+    await page.waitForTimeout(3000); // Wait for login to complete
+    await page.getByRole('button', { name: 'Share interest' }).first().click();
+    await page.waitForTimeout(2000);
+    const totalRoles = await page.getByText('Share Interest').count();
+    console.log(`Total roles available: ${totalRoles}`);
+    for (let i = 0; i < totalRoles; i++) {
+        await page.getByText('Share Interest').first().click();
+        await page.waitForTimeout(1000);
+    }
+});
